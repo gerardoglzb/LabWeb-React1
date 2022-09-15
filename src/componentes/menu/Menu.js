dd68cc2d0a48641eec768042bbcdd9e48b39d989
Gerardo González Becerril <gerbecdev@gmail.com>
2022-09-15 14:54:14 -0700

    first commit

---
+++ src/componentes/menu/Menu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+ 
+class Menu extends React.Component {
+ 
+    render() {
+   
+        return (
+   
+            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            
+            <NavLink to="/" className="navbar-brand">Mi Proyecto</NavLink>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav mr-auto">
+                </ul>
+            </div>
+
+            </nav>
+
+        )
+      
+    }
+   
+  }
+   
+  export default Menu;
